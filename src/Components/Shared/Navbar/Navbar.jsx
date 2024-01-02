a2fe9b814b8afb1199ef3cc2adf1ca5461b4e887
Fatema Khanom <fatemaurmi2019@gmail.com>
2024-01-02 12:09:41 +0600

    create a basic navbar

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,46 +1,66 @@
-import React from 'react';
+import { Link, NavLink } from "react-router-dom";
+
 
 const Navbar = () => {
+    const navOptions = <>
+    
+            <li>
+                <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "bg-gray-400 font-semibold" : ""}
+                    >Home</NavLink>
+            </li>
+            <li>
+                <NavLink
+                to="/aboutus"
+                className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "bg-gray-400 font-semibold" : ""}
+                    >About Us</NavLink>
+            </li>
+            <li>
+                <NavLink
+                to="/event"
+                className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "bg-gray-400 font-semibold" : ""}
+                    >Event</NavLink>
+            </li>
+            <li>
+                <NavLink
+                to="/blog"
+                className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "bg-gray-400 font-semibold" : ""}
+                    >Blog</NavLink>
+            </li>
+            <li>
+                <NavLink
+                to="/contactus"
+                className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "bg-gray-400 font-semibold" : ""}
+                    >Contact</NavLink>
+            </li>
+    
+     </>
     return (
-        <div>
-            <div className="navbar bg-base-100">
+        <div className="">
+            <div className="navbar bg-base-300 px-20 py-10">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {navOptions}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <p className="text-xl font-bold">DIU Competitive Programming</p>
   </div>
-  <div className="navbar-center hidden lg:flex">
+  <div className="navbar-end hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {navOptions}
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
+  
 </div>
         </div>
     );
