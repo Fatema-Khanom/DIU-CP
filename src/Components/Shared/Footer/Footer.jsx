0ac03bd3290834870b998a04376413cc3e6ad707
Fatema Khanom <fatemaurmi2019@gmail.com>
2024-01-02 10:46:29 +0600

    create a basic footer

---
+++ src/Components/Shared/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className=" footer footer-center p-4 bg-base-300 text-base-content ">
+            <aside>
+                <p>Copyright © 2024 - All right reserved by DIU CP</p>
+            </aside>
+            </footer>       
+        </div>
+    );
+};
+
+export default Footer;
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Shared/Navbar/Navbar';
+import Footer from '../Components/Shared/Footer/Footer';
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
